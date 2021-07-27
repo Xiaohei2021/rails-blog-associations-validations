613b041cf86aa44c708755f169bac52291179ff8
Leon <hualiang.tian@gmail.com>
2021-07-27 18:00:55 -0400

    commit tree 76366a197953d56c657c655b004cb8ffefcf47f3

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,17 +3,6 @@ class Post < ActiveRecord::Base
     has_many :tags, through: :post_tags
     belongs_to :user
 
-    accepts_nested_attributes_for :tags
-    
-    def tags_attributes=(tag_attributes)
-        tag_attributes.values.each do |tag_attribute|
-          unless tag_attribute[:name].blank?      
-            tag = tag.find_or_create_by(tag_attribute)
-            self.tags << tag
-          end
-        end
-      end
-
     validates :name, presence: true
     validates :content, presence: true
 end
